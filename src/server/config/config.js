8d00dd6e559342e5fec3a37ad68e7bc334156460
Desgun Wars <hernanvelasquez205@gmail.com>
2021-06-01 04:52:34 -0500

    Prueba
    Prueba montaje servidor

---
+++ src/server/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    PORT: process.env.PORT || 80,
+    DB_HOST:'',
+    DB_USER:'',
+    DB_PASSWORD: '',
+    DB_DATABASE: 'tienda',
+    BC_SALT: 10,
+    SECRET_TOKEN: '6c2cf45f9f04ad66763981e845a36ab894503e36f01805742da63'    
+}
+
